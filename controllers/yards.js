c5b184ef31cae93090cb8b5f199ab599ecf5d2ae
brandonb <brandontbyfield@gmail.com>
2025-05-20 18:37:54 -0400

    Full Crud🤔

--- controllers/yards.js
+++ controllers/yards.js
@@ -5,6 +5,7 @@ const router = express.Router();
 // Middleware used to protect routes that need a logged in user
 const ensureLoggedIn = require('../middleware/ensure-logged-in');
 const yard = require('../models/yard');
+const User = require('../models/user');
 
 // This is how we can more easily protect ALL routes for this router
 // router.use(ensureLoggedIn);
@@ -14,7 +15,6 @@ const yard = require('../models/yard');
 // index action
 router.get('/', (req, res) => {
   const yards = yard.find({})
-  console.log(yards);
   res.render('yards/index.ejs', { yards });
 });
 // GET /yards/new
@@ -33,13 +33,13 @@ router.post('/', async (req, res) => {
 });
 
 router.get('/:id', (req, res) =>{
-  console.log(req.user);
   const yard = req.user.yards.id(req.params.id);
   res.render('yards/show.ejs', { yard });
 });
 
 // Edit
 router.get('/:id/edit', (req, res) => {
+  console.log('This is req.user',req.user);
   const yard = req.user.yards.id(req.params.id);
   const statuses = User.schema.path('yards').schema.path('status').enumValues;
   res.render('yards/edit.ejs', { yard, statuses });
@@ -51,6 +51,13 @@ router.put('/:id', async (req, res) => {
   Object.assign(yard, req.body);
   await req.user.save();
   res.redirect(`/yards/${req.params.id}`);
+});
+
+// Delete
+router.delete('/:id', async (req, res) => {
+  req.user.yards.remove(req.params.id);
+  await req.user.save();
+  res.redirect('/yards');
 });
 
 module.exports = router;
